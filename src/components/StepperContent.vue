220afbf76cadbe0106a72f7a10a2fa2523ebb441
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-03-18 13:25:27 -0700

    refactored for store use

--- src/components/StepperContent.vue
+++ src/components/StepperContent.vue
@@ -1,25 +1,15 @@
 <template>
   <div class="container">
-      <h1>Step {{step}}</h1>
-      <div class="card" v-if="step === 0"><h2>Step 1</h2><button v-on:click='addStep'>Next</button></div>
-      <div class="card" v-if="step === 1"><h2>Step 2</h2><button v-on:click='addStep'>Next</button></div>
-      <div class="card" v-if="step === 2"><h2>Step 3</h2><button v-on:click='addStep'>Next</button></div>
+      <h1>Step {{$store.state.step}}</h1>
+      <div class="card" v-if="$store.state.step === 1"><h2>Step 1</h2><button v-on:click="$store.commit('addStep')">Next</button></div>
+      <div class="card" v-if="$store.state.step === 2"><h2>Step 2</h2><button v-on:click="$store.commit('addStep')">Next</button></div>
+      <div class="card" v-if="$store.state.step === 3"><h2>Step 3</h2><button v-on:click="$store.commit('addStep')">Next</button></div>
   </div>
 </template>
 
 <script>
 export default {
 name: 'StepperContent',
-data() {
-    return {
-        step: 0
-    }
-},
-methods: {
-    addStep() {
-        this.step += 1
-    }
-}
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import users from './modules/users'
+// import users from './modules/users'
 
 // Load Vuex
 Vue.use(Vuex)
 
 // Create Store
 export default new Vuex.Store({
-    modules: {
-        users
+    state: {
+        users: [],
+        step: 1
+    },
+    
+    getters: {},
+    
+    actions: {},
+    
+    mutations: {
+        addStep(state) {
+            state.step += 1
+        }
     }
-})
+    
+    
+    }
+)
