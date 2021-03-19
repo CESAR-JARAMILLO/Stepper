24b00a9650ceba27d442ed7cd0ab5c5f743e7e93
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-03-19 13:03:29 -0700

    add content and styling

--- src/components/StepperContent.vue
+++ src/components/StepperContent.vue
@@ -6,7 +6,7 @@
         </div>
 
       <div class="card" v-if="$store.state.step === 2">
-          <h2 class="is-size-2">Step 2</h2>
+          <h2 class="is-size-2">Add your name</h2>
           <p>You made agreat selection! Now lets add your name!</p>
           <label for="fname">First name</label>
           <input type="text" name="fname">
@@ -47,27 +47,48 @@ name: 'StepperContent',
     text-align: center;
     display: flex;
     flex-direction: column;
-    align-items: center; 
+    align-items: center;
+    /* justify-content: space-between; */
 }
 .card h2 {
     /* width: 260px;
     height: 46px;*/
     font-weight: 700; 
-    margin-top: 3rem;
+    margin-top: 32px ;
+    /* margin-bottom: -1rem; */
 }
 
 .card p {
   font-size: 16px;
   color: #8F92A1;
+  /* margin: 1rem; */
+  margin-top: 10px;
+}
+
+.card label {
+  /* margin-bottom: -2rem; */
+  margin-top: 38px;
+} 
+
+input {
+  width: 636px;
+  height: 58px;
+  background: #F6F8FE;
+  border: 1px solid #3050FE;
+  border-radius: 5px;
+  margin-top: 16px;
 }
 
 button {
     width: 160px;
     height: 60px;
     border-radius: 5px;
+    margin-top: 60px;
 }
 
-.back-btn {
-  float: left;
+.btn-div {
+  width: 636px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
